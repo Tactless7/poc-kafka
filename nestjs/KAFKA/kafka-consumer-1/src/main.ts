dba98a120751c888f488fbb5cd7efc21523448b7
Ben Slimen Mohamed Ali <dslimen.15@gmail.com>
2021-07-08 09:41:15 +0100

    add nestjs kafka (#1)

---
+++ nestjs/KAFKA/kafka-consumer-1/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'my-kafka-consumer-1',
+        },
+      },
+    },
+  );
+
+  app.listen(() => console.log('Kafka consumer service is listening!'));
+}
+bootstrap();
